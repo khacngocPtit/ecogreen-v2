7f5f61de5d310ba71db1bec6e4cdb75a88c8855d
khacngocPtit <khacngocit@gmail.com>
2019-08-26 15:21:52 +0700

    ...

---
+++ resources/views/master-layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Vedevi | @yield('title')</title>
+	<link rel="stylesheet" type="text/css" href="libs/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="libs/OwlCarousel2/css/owl.carousel.min.css">
+    <link rel="stylesheet" type="text/css" href="libs/OwlCarousel2/css/owl.theme.default.min.css">
+    <link rel="stylesheet" type="text/css" href="libs/WOW-master/css/animate.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.1/css/all.css" integrity="sha384-wxqG4glGB3nlqX0bi23nmgwCSjWIW13BdLUEYC4VIMehfbcro/ATkyDsF/AbIOVe" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    @yield('css')
+</head>
+<body>
+    @include('header')
+    @yield('content')
+    @include('footer')
+
+	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="libs/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/40e36cd30a.js"></script>
+    <script type="text/javascript" src="libs/OwlCarousel2/js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="libs/WOW-master/js/wow.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+
+
+    @yield('js')
+</body>
+</html>
